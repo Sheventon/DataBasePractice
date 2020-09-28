9b5c97e0e2e2f77b5325bc7ba49cb4ec83fa1773
Sheventon <tores.fernando.real@gmail.com>
2020-09-28 18:24:23 +0300

    create table students

---
+++ sql/V4_1__students_create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `db1`.`students` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `groups_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
