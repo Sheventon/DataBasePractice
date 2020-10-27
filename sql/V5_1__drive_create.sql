d6f3482f6f4e1b886632ef7fe03241f863fb3b17
chulpan22 <69673437+chulpan22@users.noreply.github.com>
2020-10-27 22:46:18 +0300

    Delete V5_1__drive_create.sql

--- sql/V5_1__drive_create.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `drive` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `client_id` INT NOT NULL,
-  `emploee_id` INT NOT NULL,
-  `car_showroom_id` SMALLINT NOT NULL,
-  `car_id` INT NOT NULL,
-  `date` DATE NOT NULL,
-  PRIMARY KEY (`id`));
